818d4b9eb51c206a8730f1a76719f1a8a20055ce
yujin <128784935+dbwlsddd@users.noreply.github.com>
2025-04-05 22:55:13 +0900

    Delete UniLife directory

--- UniLife/src/main/java/inhatc/hja/unilife/repository/EventRepository.java
+++
@@ -1,9 +0,0 @@
-// JPA 리포지토리
-
-package inhatc.hja.unilife.repository;
-
-import inhatc.hja.unilife.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
